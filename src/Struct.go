a9fac79bb10dd08ec7f1a7a147ee33f9fca8b2c3
RahulSriram <rahulrockingsriram@gmail.com>
2016-04-16 22:09:57 +0530

    Call by value & return objects

--- src/Struct.go
+++ src/Struct.go
@@ -16,6 +16,10 @@ func (p *person) GetAge int {
 	return p.age
 }
 
+func (p person) ReturnPerson person {
+	return p
+}
+
 func main() {
 	// This syntax creates a new struct.
 	fmt.Println(person{"Bob", 20})
@@ -27,7 +31,11 @@ func main() {
 	fmt.Println(&person{name: "Ann", age: 40})
 	// Access struct fields with a dot.
 	s := person{name: "Sean", age: 50}
+	p := s
+	fmt.Println(p.GetName)
+	s.name = "qwert"
 	fmt.Println(s.GetName)
+	fmt.Println(p.GetName)
 	// You can also use dots with struct pointers - the pointers are automatically dereferenced.
 	sp := &s
 	fmt.Println(sp.GetAge)
